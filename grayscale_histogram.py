9fe2c32da4c740ca6c4ae693e168cf73052af62c
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-07-03 00:54:23 +0430

    Add image opening method

--- grayscale_histogram.py
+++ grayscale_histogram.py
@@ -1,5 +1,11 @@
+from PIL import Image
+
+
 def main():
-    print("Calculating grayscale image!")
+    print("Calculating grayscale image...")
+    from PIL import Image
+    img = Image.open('image.jpg').convert('LA')  # Converting image to grayscale
+    print("Done!")
 
 
 if __name__ == "__main__":
