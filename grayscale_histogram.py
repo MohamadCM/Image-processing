f9133d22a46fcf58ea26fcc69b50fb1a13a44410
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-07-03 01:51:35 +0430

    Reformat code

--- grayscale_histogram.py
+++ grayscale_histogram.py
@@ -7,10 +7,7 @@ def main():
     print("Calculating grayscale image...")
     img = Image.open('image.jpg').convert('LA')  # Converting image to grayscale
     grayscale_array = np.asarray(img)
-    x_range = []
-    for i in range(256):
-        x_range.append(0)
-    print(grayscale_array.shape)
+    x_range = [0] * 256
     for i in range(grayscale_array.shape[0]):
         for j in range(grayscale_array.shape[0]):
             value = grayscale_array[i, j, 0]
@@ -19,7 +16,7 @@ def main():
     plt.title("Grayscale histogram")
     plt.xlabel("Intensity")
     plt.ylabel("Count")
-    plt.grid(linestyle = '--', linewidth = 0.5)
+    plt.grid(linestyle='--', linewidth=0.5)
     plt.show()
     print("Done!")
 
