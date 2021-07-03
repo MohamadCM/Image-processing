09b090d764850bcfa4ca9ff35707ad62d6ba13c5
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-07-03 18:52:46 +0430

    Debug histogram calculation problem

--- grayscale_histogram.py
+++ grayscale_histogram.py
@@ -3,16 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
-    print("Calculating grayscale image...")
-    img = Image.open('image.jpg').convert('LA')  # Converting image to grayscale
-    grayscale_array = np.asarray(img)
+def calculate_histogram(image):
+    grayscale_image = Image.open(image).convert('LA')
+    grayscale_array = np.asarray(grayscale_image)
     x_range = [0] * 256
     for i in range(grayscale_array.shape[0]):
-        for j in range(grayscale_array.shape[0]):
+        for j in range(grayscale_array.shape[1]):
             value = grayscale_array[i, j, 0]
             x_range[value] = x_range[value] + 1
-    plt.plot(np.array(x_range), "-k")
+    return x_range
+
+
+def main():
+    print("Calculating grayscale histogram...")
+    img = input("Please enter images name: ")
+    histogram = calculate_histogram(img)
+    plt.plot(np.array(histogram), "-k")
     plt.title("Grayscale histogram")
     plt.xlabel("Intensity")
     plt.ylabel("Count")
