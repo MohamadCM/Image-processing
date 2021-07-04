1d511e49ef169246421642409853b260a47e8dbc
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2021-07-04 13:32:36 +0430

    Add check for non-power of 2 numbers

--- ordered_dithering.py
+++ ordered_dithering.py
@@ -3,14 +3,27 @@ import numpy as np
 
 
 def create_threshold_map(n):
-    return (recursive_map_finder(n)) / (n * n)
+    # Converting n to closest power of 2
+    count = 0
+    if n and not (n & (n - 1)):
+        power_of_two = n
+    else:
+        while n != 0:
+            n >>= 1
+            count += 1
+        power_of_two = 1 << count
+    # Calling recursive function
+    return (recursive_map_finder(power_of_two)) / (power_of_two * power_of_two)
 
 
 def recursive_map_finder(n):
+    # Exiting condition
     if n == 2:
         return np.array([[0, 2], [3, 1]], "int")
     else:
+        # Calling recursive functions
         next_level_map = recursive_map_finder(n >> 1)
+        # Formula for threshold map
         return np.bmat(
             [
                 [4 * next_level_map, 4 * next_level_map + 2],
